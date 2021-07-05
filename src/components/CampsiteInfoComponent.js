af7d9ad9ef2f5f8c49b0df693fcbae0ee6fe342d
Veronica Adler <vlynnadler@gmail.com>
2021-07-05 14:19:41 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,8 +25,8 @@ class CommentForm extends Component {
     }
 
     ModalSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+            this.toggleModal();
+            this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -83,7 +83,7 @@ class CommentForm extends Component {
 
 }
 
- function RenderComments({comments}) {
+ function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return(
                 <div className="col-md-5 m-1">
@@ -96,7 +96,7 @@ class CommentForm extends Component {
                 )}
                 )
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
                 </div>)
         } return (<div />)
     }
@@ -130,7 +130,11 @@ function CampsiteInfo(props) {
             </div>
             <div className="row">
                 <RenderCampsite campsite={props.campsite} />
-                <RenderComments comments={props.comments} />
+                <RenderComments 
+                    comments={props.comments} 
+                    addComment={props.addComment}
+                    campsiteId={props.campsite.id}
+                />
             </div>
         </div>
             )
